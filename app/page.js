3e50215490cc35edb94838efa0a0585c7e758375
Ramaaa55 <mysticmango996@gmail.com>
2025-05-05 23:17:13 -0300

    changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
    <main>
-      <button className="btn"> Get started </button>
+    <h1> Turn Your Ideas Into Smart, Stunning Concept Maps — Instantly </h1>
+      <div> Instantly turn any text into a clear, AI-powered concept map — fast, smart, and effortless. </div>
+      <Link href="/dashboard">Dashboard</Link>
    </main>
   );
 }
