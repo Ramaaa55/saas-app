4d50091412534428a93e2f988dfcef17cd75fe00
Ramaaa55 <mysticmango996@gmail.com>
2025-05-15 13:38:36 -0300

    changes

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import ButtonLogin from "@/components/ButtonLogin";
 import ListItem from "@/components/ListItems";
 import FAQListItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "./productDemo.jpeg"
+
 
 export default function Home() {
   const isLoggedIn = true;
@@ -21,7 +24,7 @@ export default function Home() {
    <main>
     {/*HEADER*/}
     <section className="bg-base-200">
-     <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+     <div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2">
       <div className="font-bold">ConceptMapSaaS</div>
       <div className="space-x-4 max-md:hidden">
         <a className="link link-hover" href="#pricing">Pricing</a>
@@ -33,11 +36,16 @@ export default function Home() {
      </div>  
     </section>
     {/*HERO*/}
-    <section className="text-center py-32 px-8 max-w-3xl mx-auto">
-    <h1 className="text-4xl lg:text-5xl font-extrabold mb-6"> Turn Your Ideas Into Smart, Stunning Concept Maps — Instantly </h1>
+    <section className="text-center lg:text-left py-32 px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+
+      <Image src={productDemo} alt="Product demo" className="w-96 rounded-xl" />
+
+    <div>
+      <h1 className="text-4xl lg:text-5xl font-extrabold mb-6"> Turn Your Ideas Into Smart, Stunning Concept Maps — Instantly </h1>
       <div className="opacity-90 mb-10"> Instantly turn any text into a clear, AI-powered concept map — fast, smart, and effortless. </div>
 
       <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+    </div>
     
     </section>
     {/*PRICING*/}
